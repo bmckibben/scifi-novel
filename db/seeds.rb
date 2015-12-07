5efad6404a9b07bbc029c1c50e4795186b8e8ac2
bmckibben <mckibben@att.net>
2015-12-07 11:53:46 -0500

    remove default

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'Boyd-z', :email => 'bmc@literallight.com', :password => 'wm1as0De', :password_confirmation => 'wm1as0De'
-puts 'New user created: ' << user.name
